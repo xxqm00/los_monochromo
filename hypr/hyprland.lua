a1e23366459519d72ac3ca86d3a51585e0905638
xxqm00 <ki3d@proton.me>
2026-08-30 14:38:26 -0400

    Clean file of commeted lines, keep lines actively used, map of what files do what. | some '.lua' files have been reformated via VScode on accident.

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,26 +1,17 @@
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
-
-require("hyprbinds")
-require("hyprwindow")
-require("hyprstart")
+require("hyprbinds") --Where keybinds are
+require("hyprplugs") --Where hyprland plugins are (may need to be installed)
+require("hyprwindow") --Where window apperances (spacing, blur) & animations are
+require("hyprstart") --Where autostart applications are
+require("specials") --Where I store 'special:xxx' binds
 require("/dnc/submaps")
 require("/dnc/mediaNmouse")
 
-
-
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("XCURSOR_SIZE",        "24")
 hl.env("HYPRCURSOR_SIZE",     "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
 hl.env("XDG_SESSION_TYPE",    "wayland")
-hl.env("QT_STYLE_OVERRIDE",   "Orchis-Dark")  -- move this here, see below
 
-------------------
----- MONITORS ----
-------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
     output   = "",
     mode     = "preferred",
@@ -28,41 +19,12 @@ hl.monitor({
     scale    = "auto",
 })
 
-
----------------------
----- MY PROGRAMS ----
----------------------
-
--- Set programs that you use
-local terminal    = "kitty"
-local fileManager = "thunar"
-local menu        = "vicinae toggle"
-
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-
-
-----------------
-----  MISC  ----
-----------------
-
 hl.config({
     misc = {
         force_default_wallpaper = 1,    -- Set to 0 or 1 to disable the anime mascot wallpapers
         disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
     },
 })
-
-
----------------
----- INPUT ----
----------------
 
 hl.config({
     input = {
@@ -87,8 +49,7 @@ hl.gesture({
     direction = "horizontal",
     action = "workspace"
 })
---xample per-device config
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
+
 hl.device({
     name        = "epic-mouse-v1",
     sensitivity = -0.5,
